0abad3eb7b8c34755b92cf3f9881e6430bca3c6e
chien-pv <phamvanbk11@gmail.com>
2025-03-26 09:59:35 +0700

    demo reanimation

--- App.jsx
+++ App.jsx
@@ -1,18 +1,27 @@
-import { View, StyleSheet, TextInput, Button } from "react-native";
+import { View, StyleSheet, TextInput, Button, Text } from "react-native";
 import List from "./components/list";
 import { useState } from "react";
 import AddTodo from "./components/addTodo";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import Animated, { useSharedValue, withSpring } from "react-native-reanimated";
 
 function App() {
+  let width = useSharedValue(100);
+  const handlePress = () => {
+    width.value = withSpring(width.value + 50);
+  };
   return (
-    <Provider store={store}>
-      <View style={{ flex: 1 }}>
-        <AddTodo />
-        <List />
-      </View>
-    </Provider>
+    <View style={{ flex: 1, alignItems: "center" }}>
+      <Animated.View
+        style={{
+          width,
+          height: 100,
+          backgroundColor: "violet",
+        }}
+      />
+      <Button onPress={handlePress} title="Click me" />
+    </View>
   );
 }
 
